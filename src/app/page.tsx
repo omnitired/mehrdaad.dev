904756dd69caf8231051c4fa2a366854fca0a346
mehrdad <mbm1376@gmail.com>
2024-02-26 11:53:39 +0330

    test deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-lg">Mehrdaad Alizaadeh</h1>
       <h2 className="text-sm mt-4">This is gonna be my portfolio</h2>
-      
+      test deploy
     </main>
   );
 }
