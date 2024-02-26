43e15134798fddeba1197b282feaf0ea81b5b767
mehrdad <mbm1376@gmail.com>
2024-02-26 11:41:14 +0330

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-lg">Mehrdaad Alizaadeh</h1>
+      <h2 className="text-sm mt-4">This is gonna be my portfolio</h2>
+      
+    </main>
+  );
+}
